d92ff074d340c2c8516f99951baadac06efff8b3
Mikaël Van Isterdael <gpu256@hotmail.com>
2017-12-19 14:43:28 +0100

    - googlemaps - gallery still pending - mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,16 @@
 var $nav = $(".navigation");
+var $menu = $(".navigation__menu");
+var $burger = $(".burger");
 var $window = $(window);
 
 $window.scroll(function()
 {
-  let scroll = $window.scrollTop();  
+  let scroll = $window.scrollTop();
   if(scroll > 14)
     $nav.addClass("scrolled");
   else
     $nav.removeClass("scrolled");
+});
+$burger.click(function(){
+  $menu.toggleClass("activated");
 });
